3765bab92d889899185b40bc8ca0edbfedd8e020
Abdul Waheed <abdulwaheed1513@gmail.com>
2021-06-25 06:36:44 +0530

    "concept explanation experiments

--- concept-explanation/config.py
+++ concept-explanation/config.py
@@ -6,7 +6,7 @@ config = {
             "training_domain":"sst2",
             "num_classes":2, # instead of 2-class sentiment classification convert it to multiclass classfication
             "lr":2e-5, #    "lr":2e-5,
-            "batch_size":8, # 2 for test actul is 8, # large sequence length hence smaller batch size
+            "batch_size":16, # 2 for test actul is 8, # large sequence length hence smaller batch size
             "epochs":5, # 1 for testing actual is 5,
             "average":"macro",
             "max_seq_length": 512, # imdb have lengthy reviews 
